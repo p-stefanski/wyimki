0f3afa4b683a323a508646d5d6d51bde9fc568a9
ps <piotr.stefanski@wp.pl>
2023-08-06 13:18:55 +0200

    Added szara mata od chłopaków z pokoju

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <?php 
 	$photos = array(
+		"2023-07-22-01",
 		"2023-05-27-01",
 		"2023-03-29-01",
 		"2022-12-04-01",
@@ -69,7 +70,7 @@
 
 	$title = " kadry, fragmenty, wycinki";
 
-	$default_image = "2023-05-27-01";
+	$default_image = "2023-07-22-01";
 	
 	$movies = array(
 		"2015-08-01-02"
