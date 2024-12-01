8291e2dd8bae2d370c4726425171583485ab4419
PS <piotr.stefanski@wp.pl>
2024-12-01 19:59:26 +0100

    Added kapa

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <?php 
 	$photos = array(
+		"2024-11-24-01",
 		"2024-10-18-01",
 		"2024-07-03-01",
 		"2024-06-27-01",
@@ -81,7 +82,7 @@
 
 	$title = " kadry, fragmenty, wycinki";
 
-	$default_image = "2024-10-18-01";
+	$default_image = "2024-11-24-01";
 	
 	$movies = array(
 		"2015-08-01-02"
