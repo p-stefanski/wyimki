8da3517ef0be96974ef2fea36cdf1cc1fd933a6a
PS <piotr.stefanski@wp.pl>
2025-03-22 19:38:28 +0100

    Added krokodyle

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <?php 
 	$photos = array(
+		"2025-02-08-01",
 		"2025-01-11-01",
 		"2024-12-30-01",
 		"2024-12-29-01",
@@ -83,7 +84,7 @@
 
 	$title = " kadry, fragmenty, wycinki";
 
-	$default_image = "2025-01-11-01";
+	$default_image = "2025-02-08-01";
 	
 	$movies = array(
 		"2015-08-01-02"
