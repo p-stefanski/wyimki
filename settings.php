8ea97280633a42c47f01ac41ddb57365695f9b86
PS <piotr.stefanski@wp.pl>
2025-05-23 20:30:48 +0200

    Added oko dr Wszelakiego

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <?php 
 	$photos = array(
+		"2025-04-26-01",
 		"2025-04-20-01",
 		"2025-02-25-01",
 		"2025-02-08-01",
@@ -86,7 +87,7 @@
 
 	$title = " kadry, fragmenty, wycinki";
 
-	$default_image = "2025-04-20-01";
+	$default_image = "2025-04-26-01";
 	
 	$movies = array(
 		"2015-08-01-02"
